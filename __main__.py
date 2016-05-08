190776ba4699f790061663d773c2291a52cacff7
James Kerr <James Kerr>
2016-05-08 22:54:31 +1200

    Inital commit

---
+++ __main__.py
@@ -0,0 +1,49 @@
+import configparser
+import logging
+import datetime
+import os
+import shutil
+
+fileConfig = 'config.ini'
+fileLog = '{0:%Y-%m-%d}'.format(datetime.datetime.now())+".log"
+dateNow = datetime.datetime.now();
+
+#Logging and config setup
+logging.basicConfig(filename=fileLog, level=logging.INFO)
+
+config = configparser.ConfigParser()
+
+#Config file didnt exist
+if os.path.isfile(fileConfig):
+    config['SETTINGS'] = {'location': 'D:\\Downloads\\',
+                          'days': 180}
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+config.read(fileConfig)
+config.sections()
+
+#Echoing config settings
+folderLocation = config['SETTINGS'].get('location', 'D:\\Downloads\\')
+
+fileThreshold = dateNow + datetime.timedelta(days=-int(config['SETTINGS'].get('days', '180')))
+logging.info("Location:" + folderLocation)
+logging.info("Days:" + config['SETTINGS'].get('days', '180'))
+logging.info("Threshold date:" + fileThreshold.isoformat())
+
+for file in os.listdir(folderLocation):
+    creationDate = datetime.datetime.utcfromtimestamp(os.stat(folderLocation + file).st_ctime)
+    location = folderLocation + file
+    fileName = location.encode("utf-8")
+    logging.info('Found {} created at {}'.format(fileName, creationDate))
+    if creationDate < fileThreshold:
+        logging.info("{} is older than the threshold by {} days, deleting.".format(fileName, (fileThreshold - creationDate).days))
+        try:
+            if os.path.isdir(location):
+                shutil.rmtree(location)
+            elif os.path.isfile(location):
+                os.remove(location)
+            else:
+                logging.error(fileName + ' is unknown')
+        except Exception as e:
+            logging.error(e)
